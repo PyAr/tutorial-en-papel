3fce4f8c737aa0c523b95cc3296b68629c0a6f8e
Manuel Kaufmann <humitos@gmail.com>
2022-10-31 22:10:46 +0100

    Add "Comentarios iniciales" as `\section*`

--- _conf.py
+++ _conf.py
@@ -40,6 +40,34 @@ latex_elements = {
     'hyperref': r'''\usepackage[hidelinks]{hyperref}''',
     'maketitle': r'''
 \includepdf[pages=-]{../../../extra-pages/cover-front.pdf}
+
+\section*{Comentarios iniciales}
+\addcontentsline{toc}{section}{Comentarios iniciales}
+
+\sphinxAtStartPar
+En reiteradas oportunidades Python Argentina
+tradujo al castellano y editó en papel el «Tutorial de
+Python» para mayor difusión de nuestro lenguaje.
+
+\sphinxAtStartPar
+Hoy dicho tutorial, que no es otra cosa que el
+presente material, se entronca en el esfuerzo de
+numerosas personas hispanoparlantes de todo el
+mundo que han colaborado para traducir y
+disponibilizar en español la totalidad de la
+documentación (¡no sólo el tutorial!).
+
+\sphinxAtStartPar
+Con mucha alegría esperamos que este material
+rompa barreras idiomáticas y sea una puerta de
+entrada para todas aquellas personas de buena
+voluntad que se quieran sumergir e iniciar en el
+fascinante mundo del lenguaje de programación
+Python.
+
+\sphinxAtStartPar
+Para mayor información visitar:
+\sphinxurl{https://github.com/python/python-docs-es}
 ''',
     'preamble': r'''
 \usepackage{pdfpages}

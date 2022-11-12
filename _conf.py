71b434897ed99066bc67b683b41814cf459854ab
Manuel Kaufmann <humitos@gmail.com>
2022-11-12 19:25:15 +0100

    Páginas iniciales
    - título
    - copyright
    - prefacio

--- _conf.py
+++ _conf.py
@@ -25,41 +25,18 @@ latex_elements = {
     'maketitle': r'''
 \includepdf[pages=-]{../../../extra-pages/cover-front.pdf}
 
-\newpage
-
-\section*{Comentarios iniciales}
-\addcontentsline{toc}{section}{Comentarios iniciales}
-
-\sphinxAtStartPar
-En reiteradas oportunidades Python Argentina
-tradujo al castellano y editó en papel el «Tutorial de
-Python» para mayor difusión de nuestro lenguaje.
-
-\sphinxAtStartPar
-Hoy dicho tutorial, que no es otra cosa que el
-presente material, se entronca en el esfuerzo de
-numerosas personas hispanoparlantes de todo el
-mundo que han colaborado para traducir y
-disponibilizar en español la totalidad de la
-documentación (¡no sólo el tutorial!).
-
-\sphinxAtStartPar
-Con mucha alegría esperamos que este material
-rompa barreras idiomáticas y sea una puerta de
-entrada para todas aquellas personas de buena
-voluntad que se quieran sumergir e iniciar en el
-fascinante mundo del lenguaje de programación
-Python.
-
-\sphinxAtStartPar
-Para mayor información visitar:
-\sphinxurl{https://github.com/python/python-docs-es}
+% Include extra pages in LaTeX
+% Borrowed from https://github.com/jfogarty/latex-nonfiction-ebook-template/
+\input{../../../extra-pages/title.tex}
+\input{../../../extra-pages/copyright.tex}
+\input{../../../extra-pages/preface.tex}
 ''',
     'preamble': r'''
 % Make the code inside the paragraphs to be gray
 \protected\def\sphinxcode#1{\textcolor{gray}{\texttt{#1}}}
 
 \usepackage{pdfpages}
+\usepackage{changepage}
 \newif\ifsphinxverbatimwithminipage \sphinxverbatimwithminipagetrue
 ''',
     'passoptionstopackages': r'\PassOptionsToPackage{gray}{xcolor}',
